46d20cc33e70b17bec8d58aa8e41f87e6ee710df
junyi <47441677+immanuelhume@users.noreply.github.com>
2021-05-23 21:39:34 +0800

    outsource opponent response handling to own file

--- src/games/shiritori.ts
+++ src/games/shiritori.ts
@@ -1,8 +1,8 @@
-import { Message, MessageReaction, User } from "discord.js";
-import { red_cross, white_check_mark } from "../assets";
-import { genericErrorEmbed } from "../shared/genericErrorEmbed";
-import { Game, OpponentResponse } from "../types/game";
+import { Message } from "discord.js";
+import { Game } from "../types/game";
+import { handleOpponentResponse } from "./utils/handleOpponentResponse";
 import { sendNoTagError } from "./utils/sendNoTagError";
+import { sendTaggedSelfError } from "./utils/sendTaggedSelfError";
 
 class Shiritori extends Game {
   pregame(message: Message) {
@@ -13,64 +13,14 @@ class Shiritori extends Game {
       return;
     }
 
-    // if (author.id === opponent.id) {
-    //   sendTaggedSelfError(channel);
-    //   return;
-    // }
+    if (author.id === opponent.id) {
+      sendTaggedSelfError(channel);
+      return;
+    }
 
-    channel
-      .send(
-        `${opponent.toString()}! **${
-          author.username
-        }** has challenged you to a game of Shiritori!\nDo you accept this challenge?`
-      )
-      .then(async (message) => {
-        await message
-          .react(white_check_mark)
-          .then(() => message.react(red_cross));
-
-        return message
-          .awaitReactions(
-            (reaction: MessageReaction, user: User) => {
-              return (
-                user.id === opponent.id &&
-                (reaction.emoji.name === white_check_mark ||
-                  reaction.emoji.name === red_cross)
-              );
-            },
-            { time: 10000, max: 1 }
-          )
-          .then((collected): Promise<OpponentResponse> => {
-            const reaction = collected.first();
-            switch (reaction?.emoji.name) {
-              case red_cross:
-                return Promise.resolve<OpponentResponse>("rejected");
-              case white_check_mark:
-                return Promise.resolve<OpponentResponse>("accepted");
-              default:
-                return Promise.resolve<OpponentResponse>("timeout");
-            }
-          });
-      })
-      .then((response) => {
-        // TODO register the new message listener
-        switch (response) {
-          case "timeout":
-            // TODO probably do nothing
-            break;
-          case "rejected":
-            channel.send(
-              `**${opponent.username}** has turned down the challenge.`
-            );
-            break;
-          case "accepted":
-            // TODO start the game, register new message listener
-            console.log("match is gonna start");
-        }
-      })
-      .catch(() => {
-        channel.send(genericErrorEmbed);
-      });
+    handleOpponentResponse(message, opponent, () =>
+      console.log("challenge accepted")
+    );
   }
 }
 
