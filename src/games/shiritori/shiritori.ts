0efc7d4b853eb83c8978cbe20571ab62a426fc6e
junyi <47441677+immanuelhume@users.noreply.github.com>
2021-05-28 23:11:27 +0800

    refactor to client.once listener

--- src/games/shiritori/shiritori.ts
+++ src/games/shiritori/shiritori.ts
@@ -8,7 +8,7 @@ import {
 import { baseEmbed, lightErrorEmbed } from "../../shared/embeds";
 import { Game, GameType } from "../../types/game";
 import { STOP_GAME_RE } from "../utils/constants";
-import { sendGameCrashedError, sendGameStartsIn } from "../utils/embeds";
+import { sendGameStartsIn } from "../utils/embeds";
 import { handleOpponentResponse } from "../utils/handleOpponentResponse";
 import { ShiritoriGameState } from "./types";
 
@@ -41,10 +41,83 @@ export class Shiritori extends Game {
     );
   }
 
+  static createOnceListener(gameState: ShiritoriGameState) {
+    const shiritoriListener = async (message: Message) => {
+      const { author, content, channel, client } = message;
+      const noChange = Shiritori.createOnceListener(gameState);
+
+      if (
+        !(gameState.p1.id === author.id) &&
+        !(gameState.p2.id === author.id)
+      ) {
+        client.once("message", noChange);
+        return;
+      }
+      if (!(channel.id === gameState.channelID)) {
+        client.once("message", noChange);
+        return;
+      }
+
+      if (STOP_GAME_RE.test(content)) {
+        channel.send(
+          lightErrorEmbed(`**${author.username}** has stopped the game.`)
+        );
+        endGame();
+        return;
+      }
+
+      const playerCards =
+        author.id === gameState.p1.id ? gameState.p1Cards : gameState.p2Cards;
+
+      if (!content.startsWith(gameState.startingChar!)) {
+        message.react(red_cross);
+        client.once("message", noChange);
+        return;
+      }
+
+      const lastChar = content[content.length - 1];
+      if (!playerCards.includes(content[content.length - 1])) {
+        message.react(red_cross);
+        client.once("message", noChange);
+        return;
+      }
+
+      const isValidWord = await Shiritori.checkWord(content);
+      if (!isValidWord) {
+        message.react(red_cross);
+        client.once("message", noChange);
+        return;
+      }
+
+      playerCards.splice(playerCards.indexOf(lastChar), 1);
+      if (playerCards.length === 0) {
+        channel.send(
+          baseEmbed()
+            .setTitle(`**${author.username}** wins!`)
+            .setImage(chika_beating_yu_gif)
+        );
+        endGame();
+        return;
+      }
+      client.once("message", Shiritori.createOnceListener(gameState));
+      channel
+        .send(Shiritori.playerCardsEmbed(gameState))
+        .then(() => channel.send(`:regional_indicator_${lastChar}:`));
+
+      // eslint-disable-next-line no-param-reassign
+      gameState.startingChar = lastChar;
+
+      function endGame() {
+        client.gameStates.delete(channel.id);
+      }
+    };
+
+    return shiritoriListener;
+  }
+
   static startGame({ client, channel }: Message, p1: User, p2: User) {
     const [p1Cards, p2Cards, stack] = Shiritori.genInitialCards();
 
-    // TODO send some kinda start message
     const state = client.gameStates
       .set(
         channel.id,
@@ -66,91 +139,21 @@ export class Shiritori extends Game {
       message: "I'll reveal the first card in 5 seconds.",
     }).then(() => channel.send(Shiritori.playerCardsEmbed(state)));
 
-    const shiritoriListener = async (message: Message) => {
-      // this function contains the main 'loop' logic
-      const {
-        author,
-        content,
-        channel: nowChannel,
-        client: nowClient,
-      } = message;
-      const nowState = nowClient.gameStates.get(
-        nowChannel.id
-      ) as ShiritoriGameState;
-      if (!nowState) {
-        sendGameCrashedError(nowChannel);
-        endGame();
-        return;
-      }
-      if (!(nowChannel.id === nowState.channelID)) return;
-      if (!(nowState.p1.id === author.id) && !(nowState.p2.id === author.id))
-        return;
-
-      if (STOP_GAME_RE.test(content)) {
-        nowChannel.send(
-          lightErrorEmbed(`**${author.username}** has stopped the game.`)
-        );
-        endGame();
-        return;
-      }
-
-      const playerCards =
-        author.id === nowState.p1.id ? nowState.p1Cards : nowState.p2Cards;
-
-      if (!content.startsWith(nowState.startingChar!)) {
-        message.react(red_cross);
-        return;
-      }
-      const lastChar = content[content.length - 1];
-      if (!playerCards.includes(content[content.length - 1])) {
-        message.react(red_cross);
-        return;
-      }
-      const isValidWord = await Shiritori.checkWord(content);
-      if (!isValidWord) {
-        message.react(red_cross);
-        return;
-      }
-
-      playerCards.splice(playerCards.indexOf(lastChar), 1);
-      if (playerCards.length === 0) {
-        nowChannel.send(
-          baseEmbed()
-            .setTitle(`**${author.username}** wins!`)
-            .setImage(chika_beating_yu_gif)
-        );
-        endGame();
-        return;
-      }
-      nowChannel
-        .send(Shiritori.playerCardsEmbed(nowState))
-        .then(() => nowChannel.send(`:regional_indicator_${lastChar}:`));
-      nowState.startingChar = lastChar;
-    };
-
-    function endGame() {
-      client.removeListener("message", shiritoriListener);
-      client.gameStates.delete(channel.id);
-    }
-
-    function popRandom(arr: string[]) {
-      const index = Math.floor(Math.random() * arr.length);
-      return arr.splice(index, 1);
-    }
-
-    const [firstChar] = popRandom(state.stack);
+    const [firstChar] = Shiritori.popRandom(state.stack);
     state.startingChar = firstChar;
     setTimeout(() => {
       channel.send(`:regional_indicator_${firstChar}:`);
     }, 5000);
 
-    client.on("message", shiritoriListener); // register the new listener
+    client.once("message", Shiritori.createOnceListener(state)); // register the new listener
+  }
+
+  static popRandom(arr: string[]) {
+    const index = Math.floor(Math.random() * arr.length);
+    return arr.splice(index, 1);
   }
 
   static genInitialCards() {
-    // returns an array of 3 arrays
-    // first 2 contains 5 cards each for p1 and p2
-    // 3rd array contains the remaining 16 alphabets
     const allChars: string[] = [];
     for (let i = 0; i < 26; i += 1) {
       allChars.push(String.fromCharCode(i + 97));
@@ -224,5 +227,6 @@ export class Shiritori extends Game {
         value: `Be the first to clear all 5 cards!`,
       },
     ])
-    .setImage(shiritori_rules_png);
+    .setImage(shiritori_rules_png)
+    .setFooter(`chapter 188 page 4`);
 }
