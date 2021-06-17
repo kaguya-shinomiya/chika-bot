b546441c214a7924f0dc045b9382495514fa36db
junyi <47441677+immanuelhume@users.noreply.github.com>
2021-06-17 21:04:46 +0800

    add pm2

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,12 @@
 /* eslint-disable import/first */
 require("dotenv-safe").config();
 
-import cluster from "cluster";
 import Discord from "discord.js";
 import { genFullHelpEmbed } from "./init/fullHelpEmbed";
 import { initialClientCache } from "./init/initialClientCache";
 import { loadCommands } from "./init/loadCommands";
 import { loadEventListeners } from "./init/loadEventListeners";
 import { seedCommands } from "./init/seedCommands";
-import { CriticalError } from "./shared/errors";
 
 const main = () => {
   const client = new Discord.Client();
@@ -25,39 +23,30 @@ const main = () => {
   client.setMaxListeners(2048);
 };
 
-// try {
-//   main().catch((err) => {
-//     // eslint-disable-next-line no-console
-//     console.error(err);
-//     throw err;
-//   });
-// } catch (err) {
-//   // eslint-disable-next-line no-console
-//   console.error(err);
-// }
-
 process.on("unhandledRejection", (err) => {
   // eslint-disable-next-line no-console
   console.error(`Got an unhandledRejection Error ---> `, err);
   throw err;
 });
 
-// experimental
-if (cluster.isMaster) {
-  cluster.fork();
-  cluster.on("exit", (worker) => {
-    // eslint-disable-next-line no-console
-    console.error(
-      `Worker ${worker.process.pid} exited unexpectedly. Starting a new process.`
-    );
-    cluster.fork();
-  });
-} else {
-  try {
-    main();
-  } catch (err) {
-    // eslint-disable-next-line no-console
-    console.error(err);
-    if (err instanceof CriticalError) throw err;
-  }
-}
+main();
+
+// // experimental
+// if (cluster.isMaster) {
+//   cluster.fork();
+//   cluster.on("exit", (worker) => {
+//     // eslint-disable-next-line no-console
+//     console.error(
+//       `Worker ${worker.process.pid} exited unexpectedly. Starting a new process.`
+//     );
+//     cluster.fork();
+//   });
+// } else {
+//   try {
+//     main();
+//   } catch (err) {
+//     // eslint-disable-next-line no-console
+//     console.error(err);
+//     if (err instanceof CriticalError) throw err;
+//   }
+// }
