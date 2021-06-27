05ff87d5466a7373f10e010320e62ca238b05a09
junyi <47441677+immanuelhume@users.noreply.github.com>
2021-06-27 19:53:32 +0800

    stop using cron and use setInterval instead

--- src/init/cron.ts
+++ src/init/cron.ts
@@ -1,16 +1,10 @@
-import { Client } from 'discord.js';
-import { CronJob } from 'cron';
+import type { Client } from 'discord.js';
 import { activityUpdaters } from '../lib/activityUpdaters';
 
-const shuffleActivity = async () => {
-  const client = new Client();
-  await client.login(process.env.APP_TOKEN);
-  const todo =
-    activityUpdaters[Math.floor(Math.random() * activityUpdaters.length)];
-  todo(client);
-};
-const shuffleActivityCronJob = new CronJob('* */15 * * * *', shuffleActivity);
-
-export function initCrons() {
-  shuffleActivityCronJob.start();
+export function setIntervals(client: Client) {
+  client.setInterval(() => {
+    const updater =
+      activityUpdaters[Math.floor(Math.random() * activityUpdaters.length)];
+    updater(client);
+  }, 1000 * 60 * 15);
 }
